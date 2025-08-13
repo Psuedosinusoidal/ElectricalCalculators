4ba6fd05e7da3652632342dc440cee3cd45eba93
PseudoSinusoidal <zra7234@gmail.com>
2025-08-13 12:00:01 -0400

    Update CalcUtilities.py

--- src/CalcUtilities.py
+++ src/CalcUtilities.py
@@ -2,35 +2,18 @@
 import os
 from time import sleep
 
-def is_valid(*args):
-    try:
-        ii = args[0]
-        mode = args[1]
-        if mode == "float":
-            ii = float(ii) 
-            return ii
-        elif mode == "int":
-            ii = int(ii)
-            return ii
-        elif mode == "str":
-            ii = str(ii)
-            return ii
-    except (ValueError, TypeError):
-        while True:
-            try:
-                ii = input("[!] Invalid input. Try again.\n> ")
-                if mode == "float":
-                    ii = float(ii)
-                    break
-                elif mode == "int":
-                    ii = int(ii)
-                    break
-                elif mode == "str":
-                    ii = str(ii)
-                    break
-            except (ValueError, TypeError):
-                pass
-        return ii
+def is_valid(ii, mode):
+    while True:
+        try:
+            if mode == "float":
+                ii = float(ii) 
+                break
+            elif mode == "int":
+                ii = int(ii)
+                break
+        except (ValueError, TypeError):
+            ii = input("[!] Invalid input. Try again.\n> ")
+    return ii
 
 def show_menu(mm):
     sleep(1)
