19a987e55066e3e3d5a9f81f84be951335683319
PseudoSinusoidal <zra7234@gmail.com>
2025-08-15 21:57:46 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ modules = {
 
 mm = "[*] Electrical Calculators by Pseudosinusoidal\n[!] Enter the corresponding number to use module.\n[-1] Exit\n[1] 555 Timer IC\n[2] Ohm's Law\n[3] Resistor Calculator"
 
-print(mm)
+show_menu(mm)
 while True:
     ms = input("> ")
     if ms == "-1":
